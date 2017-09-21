4763e57acc466d6e1708c656f8518f367b12dcf3
Lauren Hayward Sierens <lhayward@perimeterinstitute.ca>
2017-09-21 00:11:39 -0400

    Added in Ising animation to Ising MC

--- Ising_MC/ising_mc.py
+++ Ising_MC/ising_mc.py
@@ -1,28 +1,37 @@
 # Monte Carlo for the Ising model
 
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 import numpy as np
 import random
 
 #input parameters:
 J = 1 #coupling parameter
-L = 4 #linear size of the lattice
+L = 60 #linear size of the lattice
 N_spins = L**2 #total number of spins
+
+animate = True
+bw_cmap = colors.ListedColormap(['black', 'white'])
 
 # temperature list:
 #T_list=[1.0000000000000000,1.0634592657106510,1.1269185314213019,1.1903777971319529,1.2538370628426039,1.3172963285532548,1.3807555942639058,1.4442148599745568,1.5076741256852078,1.5711333913958587,1.6345926571065097,1.6980519228171607,1.7615111885278116,1.8249704542384626,1.8884297199491136,1.9518889856597645,2.0153482513704155,2.0788075170810667,2.1422667827917179,2.2057260485023691,2.3326445799236715,2.3961038456343227,2.4595631113449739,2.5230223770556250,2.5864816427662762,2.6499409084769274,2.7134001741875786,2.7768594398982298,2.8403187056088810,2.9037779713195322,2.9672372370301834,3.0306965027408346,3.0941557684514858,3.1576150341621370,3.2210742998727881,3.2845335655834393,3.3479928312940905,3.4114520970047417,3.4749113627153929,3.5383706284260401]
-T_list = [4.0, 3.0, 2.0]
+#T_list = [4.0, 3.0, 2.0]
+T_list = np.linspace(5.0,0.2,10)
 
 initialState = 0 #0 means start from all up state, 1 means start from random state
 
-n_warmupSweeps = 10
-n_bins = 10
-n_sweepsPerBin=5
+n_warmupSweeps = 100
+n_bins = 25
+n_sweepsPerBin=1
 
 def getEnergy():
   currEnergy = 0
   for i in range(N_spins):
     currEnergy += -J*( spins[i]*spins[neighbours[i,0]] + spins[i]*spins[neighbours[i,1]] )
   return currEnergy
+
+def getMag():
+  return np.sum(spins)
 
 def sweep():
   #do one sweep (N_spins local updates):
@@ -73,7 +82,7 @@ if initialState != 0:
 
 #loop over all temperatures:
 for T in T_list:
-  print "*** T = %f ***" %T
+  print("T = %f"%T)
   
   #warm-up sweeps:
   for i in range(n_warmupSweeps):
@@ -81,12 +90,34 @@ for T in T_list:
 
   #start doing measurements:
   aveEnergy = 0
+  aveESq    = 0
+  aveMag    = 0
+  aveMSq    = 0
   for i in range(n_bins):
     for j in range(n_sweepsPerBin):
       sweep()
     #end loop over j
-    aveEnergy += getEnergy()
-  #end loop over i
+    energy = getEnergy()
+    mag    = getMag()
 
+    aveEnergy += energy
+    aveESq    += energy**2
+    aveMag    += abs(mag)
+    aveMSq    += mag**2
+  
+    if animate:
+      plt.clf()
+      plt.imshow( spins.reshape((L,L)), cmap=bw_cmap, norm=colors.BoundaryNorm([-1,0,1], bw_cmap.N) )
+      plt.xticks([])
+      plt.yticks([])
+      plt.title("%d x %d Ising model, T = %.3f" %(L,L,T))
+      plt.pause(0.01)
+  #end loop over i
+  aveEnergy = aveEnergy/(1.0*n_bins)
+  aveESq    = aveESq/(1.0*n_bins)
+  aveMag    = aveMag/(1.0*n_bins)
+  aveMSq    = aveMSq/(1.0*n_bins)
 
 #end loop over temperature
+
+plt.show()
